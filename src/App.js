e231a90a1cce7d32a9bd844ac26b3c498a54db49
phungMai234 <phungtuyet234@gmail.com>
2021-01-04 10:59:18 +0700

    task #5 router in GAS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import './App.css';
+import Home from './containers/Home';
+import Profile from './containers/Profile';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 const App = () => {
-  return <div>Asset Management</div>;
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/profile" component={Profile} />
+
+      <Redirect to="/" />
+    </Switch>
+  );
 };
 
 export default App;
